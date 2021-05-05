d545907a2162fa9bbb177f043ea3a64e3cf5d9bc
Omkar Kulkarni <omkarak45@gmail.com>
2021-05-05 14:22:34 +0530

    refactor : changed folder structure

--- client/src/common/helpers.js
+++ client/src/common/helpers.js
@@ -10,3 +10,7 @@ export function getFormattedViews(viewCount) {
 		? Math.floor(Math.abs(Number(viewCount)) / 1.0e3) + 'K'
 		: Math.abs(Number(viewCount))
 }
+
+function foo() {
+	return 'bar'
+}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultConfig')
-
+const colors = require('tailwindcss/colors')
 module.exports = {
 	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 	mode: 'jit',
@@ -10,6 +10,7 @@ module.exports = {
 				sans: ['Inter', ...defaultTheme.theme.fontFamily.sans],
 			},
 			colors: {
+				gray: colors.gray,
 				accent: '#fd4d4d',
 				'accent-hover': '#fd6868',
 				'accent-disabled': '#f5bfbf',
