38bbc20365fa2f426e27ef56c7b2dccaf85d069f
Omkar Kulkarni <omkarak45@gmail.com>
2021-05-05 21:04:32 +0530

    add : routes for add and create playlist

--- server/routes/video.route.js
+++ server/routes/video.route.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const { getVideos, getVideo } = require('../controllers/video')
+const { getVideos, getVideo, addToPlaylist, createPlaylist } = require('../controllers/video')
 const router = express.Router()
+const { checkAuth } = require('../middlewares/checkAuth')
 
 router.route('/videos').get(getVideos)
-router.route('/videos/:id').get(getVideo)
-
+router.route('/videos/:videoID').get(getVideo)
+router.route('/videos/createPlaylist/').post(checkAuth, createPlaylist)
+router.route('/videos/addToPlaylist/').post(checkAuth, addToPlaylist)
 module.exports = router
