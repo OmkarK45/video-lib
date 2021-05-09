c1c3b0516a7745ae0be80d221280350c13127705
Omkar Kulkarni <omkarak45@gmail.com>
2021-05-09 19:59:11 +0530

    feat: playlists have the cover as their first video

--- client/src/components/Playlist/PlaylistItem.jsx
+++ client/src/components/Playlist/PlaylistItem.jsx
@@ -1,20 +1,20 @@
 import { RiPlayList2Line } from 'react-icons/ri'
 import { Link } from 'react-router-dom'
 
-export default function PlaylistItem({ imgSrc, videoCount, playlistName }) {
+export default function PlaylistItem({ imgSrc, playlistName, videos }) {
 	return (
 		<div>
 			<div className="relative">
-				<img src="https://i.ytimg.com/vi/nW948Va-l10/maxresdefault.jpg" />
+				<img src={imgSrc} />
 				<div className="absolute top-0 bottom-0 right-0 flex items-center justify-center w-1/2 bg-black bg-opacity-75">
 					<div>
 						<RiPlayList2Line className="w-6 h-6 mx-auto" />
-						<p>50+ Videos</p>
+						<p>{videos.length} Videos</p>
 					</div>
 				</div>
 			</div>
 			<div className="mt-2">
-				<p className="font-medium text-white">Marvel Trailers</p>
+				<p className="font-medium text-white">{playlistName}</p>
 				<Link to="#" className="text-xs text-gray-400 uppercase">
 					View Full Playlist
 				</Link>
--- client/src/pages/Playlists.jsx
+++ client/src/pages/Playlists.jsx
@@ -1,16 +1,37 @@
 import PlaylistItem from 'components/Playlist/PlaylistItem'
 import SectionHeader from 'components/ui/SectionHeader'
 import { usePlaylist } from 'context/playlistContext'
+import { useVideo } from 'context/videoContext'
 
 export default function Playlists() {
 	const { playlistState } = usePlaylist()
+	const { videoState } = useVideo()
 	console.log(playlistState)
 	return (
 		<div className="relative text-white">
 			<SectionHeader title="Your Playlists" description="Create and manage your playlists." />
 			<div className="container grid gap-8 pt-6 mx-auto sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-				{/* {playlistState.playlists.length > 0 } */}
-				<PlaylistItem />
+				{playlistState.playlists.length > 0 ? (
+					playlistState.playlists.map((playlist) => {
+						const firstVideo = videoState.videos.data.find(
+							(v) => v._id == playlist.videos[0],
+						)
+
+						return (
+							<PlaylistItem
+								key={playlist._id}
+								imgSrc={firstVideo.thumbnail}
+								{...playlist}
+							/>
+						)
+					})
+				) : (
+					<>
+						<div className="text-white">
+							<p>You dont have any playlists</p>
+						</div>
+					</>
+				)}
 			</div>
 		</div>
 	)
